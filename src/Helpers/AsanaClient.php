026cc8d18861a70870cf1e8c86ae1d51d67c7e57
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-02-22 02:57:03 -0300

    Adding helpers for Asana

--- src/Helpers/AsanaClient.php
+++ src/Helpers/AsanaClient.php
@@ -1,7 +1,21 @@
 <?php
 namespace App\Helpers;
 
+use Asana\Client;
+
 class AsanaClient
 {
-    //put your code here
+
+    private $token;
+
+    public function setToken(string $token): AsanaClient
+    {
+        $this->token = $token;
+        return $this;
+    }
+
+    public function getClient(): Client
+    {
+        return Client::accessToken($this->token);
+    }
 }
