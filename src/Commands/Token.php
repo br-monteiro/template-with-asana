a6ccb391ed622a60d04cac287cdb8526c76e56ce
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-02-21 23:11:48 -0300

    Adding token command

---
+++ src/Commands/Token.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Commands;
+
+use splitbrain\phpcli\Options;
+use App\Bootstrap;
+use App\Helpers\FileManager as File;
+use App\Config as cfg;
+
+class Token
+{
+
+    protected $options;
+    protected $bootstrap;
+
+    public function __construct(Options $options, Bootstrap $bootstrap)
+    {
+        $this->options = $options;
+        $this->bootstrap = $bootstrap;
+    }
+
+    public function run()
+    {
+        $tokenValue = $this->options->getArgs();
+        $fileName = getcwd() . cfg::DS . cfg::FILE_CONFIG;
+        $stdToken = null;
+        
+        if (File::validateFile($fileName)) {
+            $fileContent = File::readFile($fileName);
+            $stdToken = json_decode($fileContent);
+        } else {
+            $stdToken = new \stdClass();
+        }
+        
+        $stdToken->token = $tokenValue[0] ?? null;
+        $fileContent = json_encode($stdToken);
+        
+        if (File::createFile($fileName, $fileContent)) {
+            $this->bootstrap->success("Token successfully set");
+        } else {
+            $this->bootstrap->error("Failed to save the token");
+        }
+    }
+}
